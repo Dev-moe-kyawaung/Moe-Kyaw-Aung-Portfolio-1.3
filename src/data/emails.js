7aaf671f293d4f405dbf4285d9f90ac866794cf2
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-07-12 01:39:40 +0630

    Create emails.js

---
+++ src/data/emails.js
@@ -0,0 +1,30 @@
+/**
+ * Contact email addresses.
+ * `primary` is the single address surfaced prominently across the site
+ * (contact form, footer, CTA buttons).
+ * `aliases` are additional routing addresses, shown only in the
+ * collapsed "Additional Aliases" accordion to avoid public over-exposure.
+ */
+export const primaryEmail = 'moekyawaung@engineer.com';
+
+export const emailAliases = [
+  'moekyawaung@programmer.net',
+  'moekyawaung@collector.org',
+  'moekyawaung@technologist.com',
+  'moekyawaung@techie.com',
+  'moekyawaung@graphic-designer.com',
+  'moekyawaung@cybergal.com',
+  'moekyawaung@webname.com',
+  'moekyawaung@hackermail.com',
+  'moekyawaung@graduate.org',
+  'moekyawaung@asia.com',
+  'moekyawaung@contractor.net',
+  'moekyawaung@linuxmail.org',
+  'moekyawaung@usa.com',
+  'moekyawaung@europe.com',
+  'moekyawaung@mail.com',
+  'moekyawaung@iname.com',
+  'moekyawaung@socialogist.com',
+  'moekyawaung@secretary.net',
+  'moekyawaung@publicist.com',
+];
